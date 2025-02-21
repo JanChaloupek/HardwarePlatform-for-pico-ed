eac8b3a72e2c751e289d19f191d05dae9d3d5e08
Jan Chaloupek <jan.chaloupek@unidataz.cz>
2025-02-21 23:22:37 +0100

    Podpora PinADC na pin2

--- HardwarePlatform.py
+++ HardwarePlatform.py
@@ -1,38 +1,55 @@
 # Soubor pripravujici nazvy funkci jake pouziva microbit
 from adafruit_ticks import ticks_diff as adf_ticks_diff, ticks_ms as adf_ticks_ms
+from picoed import display, i2c as pico_i2c, button_a, button_b
+from board import P0, P1, P2, P8, P12, P13, P14, P15, P19, P20
 from time import monotonic_ns, sleep as time_sleep
-from board import P0, P2, P1, P8, P12, P13, P14, P15, P19, P20
 from digitalio import DigitalInOut, Direction
-from picoed import display, i2c as pico_i2c
+from analogio import AnalogIn
 from pwmio import PWMOut
+from math import pi
+
+
+PI = pi	
+TWO_PI = 2*pi
+HALF_PI = pi/2
+
+# JoyCar konstanty
+I2C_ADDR_MOTION = 0x70
+I2C_ADDR_SENZORS = 0x38
+
+TICKS_PER_CIRCLE = 40
+WHEEL_DIAMETER = 0.067
+ROBOT_DIAMETER = 0.15
+
 
 class I2C:
     def __init__(self) -> None:
         self.picoed_i2c = pico_i2c
-        
-    def init(self, freq:int=100_000, sda=P20, scl=P19):
+
+    def init(self, freq:int=100_000, sda=P20, scl=P19) -> None:
+        # pouzivame uz existujici objekt i2c z picoed-u, takze ho nebudeme inicializovat
         pass
-    
+
     def __lock(self) -> None:
         while not self.picoed_i2c.try_lock():
             pass
 
     def __unlock(self) -> None:
         self.picoed_i2c.unlock()
-        
+
     def scan(self) -> list[int]:
         self.__lock()
         ret = self.picoed_i2c.scan()
         self.__unlock()
         return ret
-       
+
     def read(self, addr:int, n:int, repeat:bool=False) -> bytearray:
         self.__lock()
         buffer = bytearray(n)
         self.picoed_i2c.readfrom_into(addr, buffer, start=0, end=n)
         self.__unlock()
         return buffer
-    
+
     def write(self, addr:int, buf:bytearray, repeat:bool=False) -> None:
         self.__lock()
         self.picoed_i2c.writeto(addr, buf)
@@ -44,7 +61,7 @@ class PinPWM:
     def __init__(self, pin) -> None:
         self.pinName = pin
         self.pwm = None
-        
+
     def set_analog_period(self, periodMS:int) -> None:
         freq = int(1 / (periodMS / 1000))
         self.pwm = PWMOut(self.pinName, frequency=freq)
@@ -52,23 +69,36 @@ class PinPWM:
     def write_analog(self, value) -> None:
         self.pwm.duty_cycle = int(value)
 
+    def read_analog(self, value) -> None:
+        self.pwm.duty_cycle = int(value)
+
+
 class PinDigital:
     def __init__(self, pin) -> None:
         self.pinName = pin
         self.pin = DigitalInOut(self.pinName)
-        
+
     def read_digital(self) -> int:
         if self.pin.value:
             return 1
         return 0
 
     def write_digital(self, value) -> None:
-        self.pin.direction = Direction.OUTPUT    
+        self.pin.direction = Direction.OUTPUT
         self.pin.value = (value!=1)
+
+class PinADC:
+    def __init__(self, pin) -> None:
+        self.pinName = pin
+        self.pin = AnalogIn(self.pinName)
+
+    def read_analog(self) -> int:
+        print(self.pin.value)
+        return self.pin.value // 64
 
 pin0 = P0
 pin1 = PinPWM(P1)
-pin2 = PinPWM(P2)
+pin2 = PinADC(P2)
 pin8 = PinDigital(P8)
 pin12 = PinDigital(P12)
 pin13 = PinPWM(P13)
@@ -87,7 +117,7 @@ def ticks_us() -> int:
 def ticks_diff(ticks1:int, ticks2:int) -> int:
     return adf_ticks_diff(ticks1, ticks2)
 
-def time_pulse_us(pin, pulse_level: int, timeout_us=1000000) -> int:
+def time_pulse_us(pin, pulse_level: int, timeout_us=1_000_000) -> int:
     start = ticks_us()
     while pin.read_digital() != pulse_level:
         if ticks_diff(ticks_us(), start) > timeout_us:
